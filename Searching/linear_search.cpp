3fd89b72feae5e71f735feb9d745bc57ec5c210e
LazyCoder-1506 <pranayan1506@gmail.com>
2021-05-02 03:51:12 +0530

    linear search

--- Searching/linear_search.cpp
+++ Searching/linear_search.cpp
@@ -2,6 +2,22 @@
 using namespace std;
 
 int main() {
-    cout << "Hello World" << endl;
-    return 0;
+  int n;
+  cout << "Enter size of array : ";
+  cin >> n;
+  int arr[n];
+  for(int i = 0; i < n; i++) {
+    cin >> arr[i];
+  }
+  int key;
+  cout << "Enter number to search : ";
+  cin >> key;
+  for(int i = 0; i < n; i++) {
+    if(arr[i] == key) {
+      cout << "Key found at " << i << " index." << endl;
+      return 0; 
+    }
+  }
+  cout << "Key not found" << endl;
+  return 0;
 }
